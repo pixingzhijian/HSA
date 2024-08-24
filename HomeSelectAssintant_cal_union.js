5085cddec68eb10b53bfb42b9e0da733ada30bbb
LeonCH <us.er.wvv@gmail.com>
2024-08-25 00:33:09 +0800

    更新热更新， 更新合并独立文件。

--- HomeSelectAssintant_cal_union.js
+++ HomeSelectAssintant_cal_union.js
@@ -10,7 +10,7 @@
 // @grant        GM_getValue
 // @grant        GM_listValues
 // @grant        GM_deleteValue
-// @version     3.1.5
+// @version     3.1.6
 // @author      Leon
 // @description 2024/8/23 00:33:59
 
