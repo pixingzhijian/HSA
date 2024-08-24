68fdbfe849f171f4332c4881c38f5b02a8912014
LeonCH <us.er.wvv@gmail.com>
2024-08-25 00:06:35 +0800

    更新3.1.0   更新热更新， 更新合并独立文件。

--- HomeSelectAssintant_cal_union.js
+++ HomeSelectAssintant_cal_union.js
@@ -9,7 +9,7 @@
 // @grant        GM_getValue
 // @grant        GM_listValues
 // @grant        GM_deleteValue
-// @version     3.1.1
+// @version     3.1.2
 // @author      Leon
 // @description 2024/8/23 00:33:59
 
@@ -84,7 +84,7 @@
 
      // 定义不同子目录对应的脚本文件路径
      var scriptPaths = {
-         '/ershoufang/': 'http://git.nling.site/adminplokijn/HSA/raw/master/HomeSelectAssintant_cal_list.js',
+         '/ershoufang/*/': 'http://git.nling.site/adminplokijn/HSA/raw/master/HomeSelectAssintant_cal_list.js',
          '/ershoufang/*.html': 'http://git.nling.site/adminplokijn/HSA/raw/master/HomeSelectAssintant_cal_detail.js',
          '/chengjiao/': 'http://git.nling.site/adminplokijn/HSA/raw/master/HomeSelectAssintant_cal_deal.js',
          // 更多子目录和对应的脚本文件...
