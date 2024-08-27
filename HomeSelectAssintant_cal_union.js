dea462c7d67eeaee9aa6c5fa6337498e59005851
LeonCH <us.er.wvv@gmail.com>
2024-08-28 05:03:10 +0800

    完成 小区成交批量查询 与页面显示！！ 新增推荐房源检测并查询！
     查询在售页面列表，插入小区信息。  同步对象LIST循环和组件循环。
    
    Signed-off-by: LeonCH <us.er.wvv@gmail.com>

--- HomeSelectAssintant_cal_union.js
+++ HomeSelectAssintant_cal_union.js
@@ -10,16 +10,18 @@
 // @grant        GM_getValue
 // @grant        GM_listValues
 // @grant        GM_deleteValue
-// @version     3.1.11
+// @version     4.0.0
 // @author      Leon
 // @description 2024/8/23 00:33:59
 // @downloadURL http://git.nling.site/adminplokijn/HSA/raw/master/HomeSelectAssintant_cal_union.js
 
 // @note 6.0.0 TODO:运用模型选房，运用机器学习模型进行评分
-// @note 5.0.0 TODO:快捷可视化分析
-// @note 4.0.0 TODO:远端保存
+// @note 5.0.1 TODO:快捷可视化分析
+// @note 5.0.0 TODO: 远端保存缓存数据，减轻服务器压力
+// @note 4.0.0 完成 小区成交批量查询 与页面显示！！ 新增推荐房源检测并查询！
 
-// @note 3.1.0 更新热更新， 更新合并独立文件。。
+
+// @note 3.1.0 更新热更新， 更新合并独立文件。
 
 
 // @note 3.0.5 新增实际单价折扣率， 用于展示建筑面积和套内面积的实际房价单价的增长率。
