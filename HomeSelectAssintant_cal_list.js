177cdcf549a29c8f4289bf8e85fdd12f78e1f80b
LeonCH <us.er.wvv@gmail.com>
2024-08-28 05:11:45 +0800

    // @note 4.0.0 修复小区缓存问题
    Signed-off-by: LeonCH <us.er.wvv@gmail.com>

--- HomeSelectAssintant_cal_list.js
+++ HomeSelectAssintant_cal_list.js
@@ -807,7 +807,10 @@
 
         // 添加内容展示 deal_info_list 中每个对象的所有键值对
         let content = document.createElement('div');
-        content.innerHTML = `<h3>详细信息    ${deal_info_list.is_positionId_catch}</h3>`;
+        if (deal_info_list.is_positionId_catch) {
+            content.innerHTML = `<h3>详细信息   (${deal_info_list.is_positionId_catch})</h3>`;
+        }
+        content.innerHTML = `<h3>详细信息   (实时)</h3>`;
         deal_info_list.forEach(info => {
             let itemDiv = document.createElement('div');
             itemDiv.innerHTML = '<strong></strong>';
